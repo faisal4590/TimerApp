ce9ecf54c14566b4798ea6ff3c2a3b4419665874
optimized faisal <optimized faisal>
2017-01-07 15:21:55 +0600

    Creating our router

---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <p>timer.jsx</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Timer;
