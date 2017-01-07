8408b2bc43cf697a92c879c172c79396d48607e4
optimized faisal <optimized faisal>
2017-01-07 14:39:20 +0600

    first test

---
+++ app/tests/app.test.jsx
@@ -0,0 +1,17 @@
+var expect = require('expect');
+
+/*
+* michael jackson er expect library ke include kore nilam...
+* */
+describe('App',()=>{
+    it('should properly run tests',()=>{
+        expect(1).toBe(1);
+    });
+    /*
+     * it hocce mocha er 1ta command.. eta die mainly check korteci
+     * 1 pele seta 1 i hobe...
+     * describe use korlam tahole cmd te sundor format e test
+     * result output dibe......
+     * */
+});
+
