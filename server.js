ad05f1363d974c0635f9f2dcb5bb1f25b3f83679
optimized faisal <optimized faisal>
2017-01-14 18:26:46 +0600

    fixed issue

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;//heroku te to 5000 port e always cholbena..
+//tai port specify kore dilam or 3000 port e cholbe..
 
-app.use(function (req, res, next){
-  if (req.headclsers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
+app.use(function (request, response, next) {
+    /*.......This part is for Deploying our app to heroku......
+    * amader app ta http te chole.. https e run hobena..
+    *tai check korbo jodi http thake tahole next() e agabo..
+    * ar jodi https thake tahole take redirect kore http kore dibo...
+    * */
+    if (request.headers['x-forwarded-proto'] === 'https') {
+        response.redirect('http://' + request.hostname + request.url);
+    }
+    else {
+        next();
+    }
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
+    console.log('Express server is up on port ' + PORT);
 });
-
